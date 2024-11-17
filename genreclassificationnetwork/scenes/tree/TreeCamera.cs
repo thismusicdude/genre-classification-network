942f260d9626a316248a0907c90d93bf8f4b323f
Max Schmitt <max98tonton@gmail.com>
2024-11-17 16:43:17 +0100

    wip camera stuff

--- genreclassificationnetwork/scenes/tree/TreeCamera.cs
+++ genreclassificationnetwork/scenes/tree/TreeCamera.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class TreeKamera : Camera3D
+public partial class TreeCamera : Marker3D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -11,5 +11,27 @@ public partial class TreeKamera : Camera3D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+	
+	public override void _Input(InputEvent @event)
+	{
+		// Überprüft, ob die rechte Pfeiltaste gedrückt wird
+		if (@event.IsActionPressed("ui_right"))
+		{
+			RotateY(-RotationSpeed * GetProcessDeltaTime());
+		}
+		
+		// Überprüft, ob die linke Pfeiltaste gedrückt wird
+		if (@event.IsActionPressed("ui_left"))
+		{
+			RotateY(RotationSpeed * GetProcessDeltaTime());
+		}
+		
+		// Beispiel für Mausbewegungen
+		if (@event is InputEventMouseMotion mouseEvent)
+		{
+			RotateY(-mouseEvent.Relative.x * RotationSpeed * 0.01f);
+			RotateX(-mouseEvent.Relative.y * RotationSpeed * 0.01f);
+		}
 	}
 }
--- genreclassificationnetwork/scenes/tree/VisualizationTree.cs
+++ genreclassificationnetwork/scenes/tree/VisualizationTree.cs
@@ -3,13 +3,39 @@ using System;
 
 public partial class VisualizationTree : Node3D
 {
+	
+	[Export]
+	public double rotationSpeed = 1.0;
+
+	private Camera3D camera;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		camera = GetNode<Camera3D>("Camera3D");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
+		if (Input.IsActionPressed("ui_right"))
+		{
+			RotateY(-rotationSpeed * delta);
+		}
+		if (Input.IsActionPressed("ui_left"))
+		{
+			RotateY(rotationSpeed * delta);
+		}
 	}
+
+    private void RotateY(double v)
+    {
+		camera.RotateY()
+        throw new NotImplementedException();
+    }
+
 }
+
+
+
