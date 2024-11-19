311da6b7418fb7512863c98f067e01c4fd2e3865
Max Schmitt <max98tonton@gmail.com>
2024-11-19 23:59:41 +0100

    GerneTreeNode Script, TreeCamera script

---
+++ genreclassificationnetwork/scenes/tree/GenreTreeNodeCollection.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+
+namespace MusicGenreTreeView {
+	public partial class GenreTreeNodeCollection : Node3D
+	{
+
+		PackedScene genreTreeNode = (PackedScene)ResourceLoader.Load("res://scenes/tree/GenreTreeNode.tscn");
+
+		public void AddGenreNode(string title, Vector3 pos, float size, Color color){
+			GenreTreeNode newNode = genreTreeNode.Instantiate<GenreTreeNode>() as GenreTreeNode;
+			
+			newNode.SetGenreTitle(title);
+			newNode.SetSize(size);
+			newNode.SetSphereColor(color);
+
+    		this.AddChild(newNode);
+		}
+	}
+}
--- genreclassificationnetwork/scenes/tree/TreeCamera.cs
+++ genreclassificationnetwork/scenes/tree/TreeCamera.cs
@@ -1,37 +1,32 @@
 using Godot;
 using System;
 
-public partial class TreeCamera : Marker3D
-{
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+namespace MusicGenreTreeView {
+	public partial class TreeCamera : Marker3D
 	{
-	}
-	
-	public override void _Input(InputEvent @event)
-	{
-		// Überprüft, ob die rechte Pfeiltaste gedrückt wird
-		if (@event.IsActionPressed("ui_right"))
+		
+		[Export]
+		public float rotationSpeed = 1;
+		
+		// Called when the node enters the scene tree for the first time.
+		public override void _Ready()
 		{
-			RotateY(-RotationSpeed * GetProcessDeltaTime());
+		}
+
+		// Called every frame. 'delta' is the elapsed time since the previous frame.
+		public override void _Process(double delta)
+		{
+			float delta_f =  Convert.ToSingle(delta);
+			if (Input.IsActionPressed("ui_right"))
+			{
+				RotateY(-rotationSpeed * delta_f);
+			}
+			if (Input.IsActionPressed("ui_left"))
+			{
+				RotateY(rotationSpeed * delta_f);
+			}
 		}
 		
-		// Überprüft, ob die linke Pfeiltaste gedrückt wird
-		if (@event.IsActionPressed("ui_left"))
-		{
-			RotateY(RotationSpeed * GetProcessDeltaTime());
-		}
-		
-		// Beispiel für Mausbewegungen
-		if (@event is InputEventMouseMotion mouseEvent)
-		{
-			RotateY(-mouseEvent.Relative.x * RotationSpeed * 0.01f);
-			RotateX(-mouseEvent.Relative.y * RotationSpeed * 0.01f);
-		}
 	}
 }
