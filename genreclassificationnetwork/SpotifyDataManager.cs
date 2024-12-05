12aa7af1dd740f7aedd2f0394b9255721d872f0b
Luca Stehle <luca.stehle@stud.h-da.de>
2024-12-05 15:49:16 +0100

    load spotify account data

--- genreclassificationnetwork/SpotifyDataManager.cs
+++ genreclassificationnetwork/SpotifyDataManager.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Godot;
+using Newtonsoft.Json;
 
 public partial class SpotifyDataManager : Node
 {
@@ -19,4 +25,43 @@ public partial class SpotifyDataManager : Node
 			QueueFree();
 		}
 	}
+
+/*
+	// API-Aufruf: Top-Künstler abrufen
+	public async Task<string> GetTopArtists(string accessToken)
+	{
+		using (HttpClient client = new HttpClient())
+		{
+			client.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
+			var response = await client.GetAsync("https://api.spotify.com/v1/me/top/artists?limit=50");
+			return await response.Content.ReadAsStringAsync();
+		}
+	}
+
+	// Genres aus Top-Künstlern extrahieren
+	public async Task<List<string>> GetTopGenres(string accessToken)
+	{
+		string artistData = await GetTopArtists(accessToken);
+		var artistResponse = JsonConvert.DeserializeObject<dynamic>(artistData);
+
+		// Genres sammeln
+		var genreList = new List<string>();
+		foreach (var artist in artistResponse.items)
+		{
+			foreach (var genre in artist.genres)
+			{
+				genreList.Add((string)genre);
+			}
+		}
+
+		// Doppelte Genres entfernen und nach Häufigkeit sortieren
+		var topGenres = genreList
+			.GroupBy(g => g)
+			.OrderByDescending(g => g.Count())
+			.Select(g => g.Key)
+			.ToList();
+
+		return topGenres;
+	}
+	*/
 }
--- genreclassificationnetwork/scenes/menu/LogIn.cs
+++ genreclassificationnetwork/scenes/menu/LogIn.cs
@@ -24,64 +24,130 @@ namespace GenreTreeMenu {
 				logInButton.Connect(Button.SignalName.Pressed, Callable.From(_OnLogInButtonPressed));
 				GD.Print("logInButton verbunden!");
 			}
-		}
-
-		public async Task _OnLogInButtonPressed()
-		{
-			GD.Print("LogIn-Button wurde gedrückt!"); 
 			
-			string scope = "user-read-private user-read-email";
-			string authUrl = $"https://accounts.spotify.com/authorize?response_type=code&client_id={clientId}&redirect_uri={Uri.EscapeDataString(redirectUri)}&scope={Uri.EscapeDataString(scope)}";
-
-			OpenWebsite(authUrl);
-
-			// Warte auf den Authentifizierungscode
-			string code = "AUTHORIZATION_CODE"; // Ersetze durch tatsächlichen Code
-			string accessToken = await GetAccessToken(code);
-
-			SpotifyDataManager.Instance.AccessToken = accessToken;
-
-			string profileData = await GetUserProfile(accessToken);
-			SpotifyDataManager.Instance.UserProfileData = profileData;
-
-			GD.Print("Benutzerprofil geladen: ", profileData);
-		}
-
-		public void OpenWebsite(string url)
-		{
-			OS.ShellOpen(url);
-		}
-
-		public async Task<string> GetAccessToken(string code)
-		{
-			using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
+			/*
+			var loadGenresButton = GetNode<Button>("LoadGenresButton");
+			if (loadGenresButton != null)
 			{
-				var request = new HttpRequestMessage(HttpMethod.Post, "https://accounts.spotify.com/api/token");
-				request.Content = new FormUrlEncodedContent(new[]
-				{
-					new KeyValuePair<string, string>("grant_type", "authorization_code"),
-					new KeyValuePair<string, string>("code", code),
-					new KeyValuePair<string, string>("redirect_uri", redirectUri),
-					new KeyValuePair<string, string>("client_id", clientId),
-					new KeyValuePair<string, string>("client_secret", clientSecret),
-				});
-
-				var response = await client.SendAsync(request);
-				var responseString = await response.Content.ReadAsStringAsync();
-				var tokenData = JsonConvert.DeserializeObject<dynamic>(responseString);
-
-				return tokenData.access_token;
+				loadGenresButton.Connect(Button.SignalName.Pressed, Callable.From(DisplayTopGenres));
+				GD.Print("LoadGenresButton verbunden!");
 			}
+			*/
 		}
-
-		public async Task<string> GetUserProfile(string accessToken)
+		
+	// Öffnet eine URL im Standardbrowser
+	public void OpenWebsite(string url)
+	{
+		OS.ShellOpen(url);
+	}
+	
+	// Holt das Access Token von Spotify
+	public async Task<string> GetAccessToken(string code)
+	{
+		using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
 		{
-			using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
+			var request = new HttpRequestMessage(HttpMethod.Post, "https://accounts.spotify.com/api/token");
+			request.Content = new FormUrlEncodedContent(new[]
 			{
-				client.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
-				var response = await client.GetAsync("https://api.spotify.com/v1/me");
-				return await response.Content.ReadAsStringAsync(); // JSON-Daten des Benutzerprofils
-			}
+				new KeyValuePair<string, string>("grant_type", "authorization_code"),
+				new KeyValuePair<string, string>("code", code),
+				new KeyValuePair<string, string>("redirect_uri", redirectUri),
+				new KeyValuePair<string, string>("client_id", clientId),
+				new KeyValuePair<string, string>("client_secret", clientSecret),
+			});
+
+			var response = await client.SendAsync(request);
+			var responseString = await response.Content.ReadAsStringAsync();
+			var tokenData = JsonConvert.DeserializeObject<dynamic>(responseString);
+
+			return tokenData.access_token;
 		}
 	}
+	
+	// Holt das Benutzerprofil von Spotify
+	public async Task<string> GetUserProfile(string accessToken)
+	{
+		using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
+		{
+			client.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
+			var response = await client.GetAsync("https://api.spotify.com/v1/me");
+			return await response.Content.ReadAsStringAsync(); // JSON-Daten des Benutzerprofils
+		}
+	}
+
+	public async Task _OnLogInButtonPressed()
+	{
+		GD.Print("LogIn-Button wurde gedrückt!");
+
+		string scope = "user-read-private user-read-email";
+		string authUrl = $"https://accounts.spotify.com/authorize?response_type=code&client_id={clientId}&redirect_uri={Uri.EscapeDataString(redirectUri)}&scope={Uri.EscapeDataString(scope)}";
+
+		// Öffne Spotify-Authentifizierungsseite
+		OpenWebsite(authUrl);
+
+		// Starte lokalen Server und warte auf den Authentifizierungscode
+		var server = new LocalServer("http://localhost:5000/callback/");
+		string code = await server.WaitForCodeAsync();
+
+		// Debug-Ausgabe des Codes
+		GD.Print($"Empfangener Authentifizierungscode: {code}");
+
+		if (string.IsNullOrEmpty(code))
+		{
+			GD.PrintErr("Kein Authentifizierungscode empfangen!");
+			return;
+		}
+
+		// Zugriffstoken anfordern
+		string accessToken = await GetAccessToken(code);
+		if (string.IsNullOrEmpty(accessToken))
+		{
+			GD.PrintErr("Kein Zugriffstoken erhalten!");
+			return;
+		}
+		
+		GD.Print($"Empfangenes Access Token: {accessToken}");
+
+		
+		if (SpotifyDataManager.Instance == null)
+		{
+			GD.PrintErr("SpotifyDataManager.Instance ist null!");
+			return;
+		}
+		else
+		{
+			GD.Print("SpotifyDataManager.Instance existiert.");
+		}
+
+		SpotifyDataManager.Instance.AccessToken = accessToken;
+		
+		// Benutzerprofil abrufen
+		string profileData = await GetUserProfile(accessToken);
+
+		GD.Print($"Benutzerprofil: {profileData}");
+		SpotifyDataManager.Instance.UserProfileData = profileData;
+
+		GD.Print($"Benutzerprofil geladen: {profileData}");
+		}
+}
+	
+
+/*
+public async Task DisplayTopGenres()
+{
+	if (SpotifyDataManager.Instance == null || string.IsNullOrEmpty(SpotifyDataManager.Instance.AccessToken))
+	{
+		GD.PrintErr("SpotifyDataManager ist nicht initialisiert oder kein AccessToken vorhanden.");
+		return;
+	}
+
+	List<string> topGenres = await SpotifyDataManager.Instance.GetTopGenres(SpotifyDataManager.Instance.AccessToken);
+
+	GD.Print("Deine Top-Genres:");
+	foreach (var genre in topGenres)
+	{
+		GD.Print(genre);
+	}
+}
+*/
 }
